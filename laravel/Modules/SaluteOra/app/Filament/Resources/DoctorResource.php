6e0d1a2cf8cf625cb0d6669f0cb6d9df1056605c
marco76tv <marco.sottana@gmail.com>
2025-06-12 23:18:57 +0200

    ♻️ (DoctorResource.php): refactor unique validation to enable ignoring the current record for email uniqueness check

--- laravel/Modules/SaluteOra/app/Filament/Resources/DoctorResource.php
+++ laravel/Modules/SaluteOra/app/Filament/Resources/DoctorResource.php
@@ -482,7 +482,7 @@ class DoctorResource extends XotBaseResource
                             ->maxLength(255)
                             ->autocomplete('email')
                             //->unique(User::class)
-                            //->unique(ignoreRecord: true)
+                            ->unique(ignoreRecord: true)
                             ,
                         /*
                         'certifications' => Forms\Components\FileUpload::make('certifications')
