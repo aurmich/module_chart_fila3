5bf748914614b6e20f5c23dade6ebb6867e4440f
marco76tv <marco.sottana@gmail.com>
2025-06-24 10:00:53 +0200

    .

--- laravel/Modules/Notify/lang/it/send_email.php
+++ laravel/Modules/Notify/lang/it/send_email.php
@@ -51,6 +51,11 @@ return array (
       'placeholder' => 'Inserisci i parametri',
       'label' => 'Parametri',
     ),
+<<<<<<< HEAD
+<<<<<<< HEAD
+=======
+=======
+>>>>>>> 484b1fe (.)
     'body_html' => 
     array (
       'description' => 'body_html',
@@ -70,8 +75,13 @@ return array (
     'subject' => 
     array (
       'description' => 'subject',
+<<<<<<< HEAD
+    ),
+>>>>>>> 86996d7 (.)
+=======
       'helper_text' => 'subject',
     ),
+>>>>>>> 484b1fe (.)
   ),
   'actions' => 
   array (
