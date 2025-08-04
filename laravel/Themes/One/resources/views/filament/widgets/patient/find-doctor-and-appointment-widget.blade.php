4603bc842a3a4a546fe42458248b08c7052c36c0
marco76tv <marco.sottana@gmail.com>
2025-08-04 15:09:03 +0200

    .

--- laravel/Themes/One/resources/views/filament/widgets/patient/find-doctor-and-appointment-widget.blade.php
+++ laravel/Themes/One/resources/views/filament/widgets/patient/find-doctor-and-appointment-widget.blade.php
@@ -1,4 +1,5 @@
 <<<<<<< HEAD
+<<<<<<< HEAD
 <x-filament-widgets::widget>
 <<<<<<< HEAD
 
@@ -9,6 +10,10 @@
 <x-filament-widgets::widget class="bg-[#E6EBF7]">
 
 >>>>>>> 3ad9917a (- fix styles registration)
+=======
+<x-filament-widgets::widget>
+    <x-filament::section>
+>>>>>>> 3e5b3e0a (✨ (find-doctor-and-appointment-widget): introduce a new widget for finding doctors and booking appointments with an enhanced user experience)
         <div class="max-w-4xl mx-auto">
             <form wire:submit.prevent="register" class="space-y-6">
                 {{ $this->form }}
@@ -16,9 +21,13 @@
         </div>
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
 =======
     </x-filament::section>
 >>>>>>> aurmich/dev
 =======
 >>>>>>> 3ad9917a (- fix styles registration)
+=======
+    </x-filament::section>
+>>>>>>> 3e5b3e0a (✨ (find-doctor-and-appointment-widget): introduce a new widget for finding doctors and booking appointments with an enhanced user experience)
 </x-filament-widgets::widget>
