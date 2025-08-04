b4832fce3bb6052a2e1c788eac9e2bdd0847bd31
marco76tv <marco.sottana@gmail.com>
2025-08-04 15:12:17 +0200

    .

--- laravel/Modules/Notify/app/Emails/SpatieEmail.php
+++ laravel/Modules/Notify/app/Emails/SpatieEmail.php
@@ -157,6 +157,7 @@ class SpatieEmail extends TemplateMailable
         ]);
         
         $data=$record->toArray();
+        $this->data['logo_header']=MetatagData::make()->getBrandLogo();
         $this->data=array_merge($this->data,$data);
         $this->setAdditionalData($this->data);
         
