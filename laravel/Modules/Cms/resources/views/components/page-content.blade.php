f4bb52fa514e2467e51b52e1a63e8b76d17d7f75
marco76tv <marco.sottana@gmail.com>
2025-08-04 15:06:39 +0200

    .

--- laravel/Modules/Cms/resources/views/components/page-content.blade.php
+++ laravel/Modules/Cms/resources/views/components/page-content.blade.php
@@ -1,10 +1,16 @@
 @foreach($blocks as $block)
 <<<<<<< HEAD
+<<<<<<< HEAD
 =======
     {{--
     <x-dynamic-component component="blocks.ticket-list.agid" />
     --}}
 >>>>>>> 54f4fa16 (.)
+=======
+    {{--
+    <x-dynamic-component component="blocks.ticket-list.agid" />
+    --}}
+>>>>>>> 8e074dc7 (.)
     @include($block->view,$block->data)
 @endforeach
 
