a0dc53d476452b2e0bf6327a308ce42e254b494c
Malebestia <malebestia@yahoo.it>
2025-08-26 17:58:32 +0200

    .

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -22,18 +22,8 @@ class AdminPanelProvider extends XotBasePanelProvider
         //    Js::make('chart-js-plugins', Vite::asset('resources/js/filament-chart-js-plugins.js', 'assets/chart'))->module(),
         // ]);
         FilamentAsset::register([
-<<<<<<< HEAD
-<<<<<<< HEAD
-            Js::make('chart-js-plugins', Vite::asset('resources/js/filament-chart-js-plugins.js', 'assets/chart'))->module(),
-            Css::make('chart-js-plugins', Vite::asset('resources/css/app.css', 'assets/chart')),
-=======
             Css::make('chart-js-plugins', Vite::asset('resources/css/app.css', 'assets/chart')),
             Js::make('chart-js-plugins', Vite::asset('resources/js/filament-chart-js-plugins.js', 'assets/chart'))->module(),
->>>>>>> 359b1c2 (.)
-=======
-            Css::make('chart-js-plugins', Vite::asset('resources/css/app.css', 'assets/chart')),
-            Js::make('chart-js-plugins', Vite::asset('resources/js/filament-chart-js-plugins.js', 'assets/chart'))->module(),
->>>>>>> bf68c71 (.)
         ]);
 
         return $panel;
