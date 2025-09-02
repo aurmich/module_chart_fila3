2c618a7fe143fb246e2ff5ea1ff4e99540e0bd3c
marco76tv <marco.sottana@gmail.com>
2025-09-02 08:31:53 +0200

    .

--- bashscripts/fix/fix_file_perms.sh
+++ bashscripts/fix/fix_file_perms.sh
@@ -3,6 +3,7 @@ git config core.filemode false
 sudo chown -R www-data:www-data .
 sudo find . -type f -exec chmod 644 {} \;
 sudo find . -type d -exec chmod 755 {} \;
+<<<<<<< HEAD
 sudo chmod -R u+w .git
 # Fix the permissions on your SSH config file
 chmod 600 ~/.ssh/config
@@ -10,3 +11,6 @@ chmod 600 ~/.ssh/config
 chmod 644 ~/.ssh/id_rsa.pub
 # Also ensure your .ssh directory has correct permissions
 chmod 700 ~/.ssh
+=======
+sudo chmod -R u+w .git
+>>>>>>> 8345b0c130 (.)
